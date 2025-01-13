723832713f8594ced832dfa0b2d8a5250e65863f
blerdline <blerdline@gmail.com>
2025-01-13 10:17:29 -0600

    Add preliminary directory class

---
+++ directory.py
@@ -0,0 +1,13 @@
+class Directory:
+    def __init__(self, name):
+        self.name = name
+        self.subdirectories = {}
+
+    def add_subdirectory(self, name):
+        pass
+
+    def delete_subdirectory(self, name):
+        pass
+
+    def get_subdirectories(self, name):
+        pass
