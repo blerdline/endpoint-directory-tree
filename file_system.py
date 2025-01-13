aecf32e5e302a4849e9994092bc9045a57a15586
blerdline <blerdline@gmail.com>
2025-01-13 11:26:30 -0600

    Add create function for file

--- file_system.py
+++ file_system.py
@@ -7,7 +7,20 @@ class FileSystem:
         self.root = Directory("")
 
     def create(self, path: str) -> None:
-        pass
+
+        #Split the path into parts
+        parts: list[str] = path.split('/')
+
+        #Set the base directory to the root
+        current_directory: Directory = self.root
+
+        # Add each part of the path to the directory by adding the first part
+        # then setting the current directory to the subdirectory we just added
+        # repeat until we reach the end of the path
+        for part in parts:
+            current_directory.add_subdirectory(part)
+            current_directory = current_directory.get_subdirectories(part)
+        
 
     def delete(self, path: str) -> None:
         pass
